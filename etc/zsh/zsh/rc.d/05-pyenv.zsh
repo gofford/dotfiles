6c585b2f6afe2e3425b4222b6df128679e240770
gofford <jason@yotoplay.com>
2024-01-02 09:39:24 +0000

    fix: simplify pyenv load

--- etc/zsh/zsh/rc.d/05-pyenv.zsh
+++ etc/zsh/zsh/rc.d/05-pyenv.zsh
@@ -4,8 +4,6 @@
 # Invoke pyenv
 #
 
-__command_is_absent pyenv && {
-	export PYENV_ROOT="$HOME/.pyenv"
-	command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-	eval "$(pyenv init -)"
-}
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
